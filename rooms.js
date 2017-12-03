75ccecf4274487ad8adadaa3d23ae01afef94cdf
relick <drrintofen@gmail.com>
2017-12-03 17:37:03 +0000

    testfix7

--- rooms.js
+++ rooms.js
@@ -84,7 +84,7 @@ function PlayerManager(maxSize) {
                     });
                 }
                 text += '</ul></div>';
-                text += '<div id="options"><a href="javascript:;" onclick="startRoom()"Start room</a></div>';
+                text += '<div id="options"><a href="javascript:;" onclick="startRoom()">Start room</a></div>';
                 this.freePls[i].emit('update', {state:"PREGAME", html:text});
             }
         }
