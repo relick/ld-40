86a152f1feda64b65df9bc375b03084166caf1e8
relick <drrintofen@gmail.com>
2017-12-03 17:54:21 +0000

    testfix10

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var socket = io();
 
 socket.on('update', update);
-
+var n = 1;
 function update(data) {
+    console.log(n);
+    n+=1;
     if(data.state === "PREGAME") {
         $("#mid").html($(data.html));
     } else {
